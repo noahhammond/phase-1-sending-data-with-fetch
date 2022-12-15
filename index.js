28d373b6502f9013d6978f49073da906126e8f5c
Noah <business.noah@hotmail.com>
2022-12-15 01:56:59 -0700

    commit tree c0d529c91598774e6735da802f393251896604f8

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(name, email) {
+    const configObj = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',},
+            body: JSON.stringify(
+                {name: `${name}`,
+                email: `${email}`,
+            })
+        }
+        return fetch('http://localhost:3000/users', configObj)
+        .then(resp => resp.json())
+        .then(newObj => document.querySelector('body').innerHTML = newObj.id)
+        .catch(error => document.querySelector('body').innerHTML = error)
+    }
